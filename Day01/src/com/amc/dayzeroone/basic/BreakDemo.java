97d9fd7fd652c970b60017acda967f730abd84a3
Naveen <Naveen@LAPTOP-UVCG3BR0>
2024-01-09 18:28:46 +0530

    second commit

---
+++ Day01/src/com/amc/dayzeroone/basic/BreakDemo.java
@@ -0,0 +1,9 @@
+package com.amc.dayzeroone.basic;
+
+public class BreakDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("the element is: " + i); // Corrected usage of println
+        }
+    }
+}
